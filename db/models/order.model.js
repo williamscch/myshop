fc808bf357ca80f2c568a23ee4eb6d4fd82accec
Williams Colmenares <david.13.2121@gmail.com>
2023-04-04 15:48:35 -0400

    manage relations between orders and products

--- db/models/order.model.js
+++ db/models/order.model.js
@@ -1,5 +1,5 @@
 const { Model, DataTypes, Sequelize } = require("sequelize");
-const { CUSTOMER_TABLE, Customer } = require("./customer.model");
+const { CUSTOMER_TABLE } = require("./customer.model");
 
 const ORDER_TABLE = "orders";
 
@@ -18,6 +18,8 @@ const OrderSchema = {
       model: CUSTOMER_TABLE,
       key: "id",
     },
+    onUpdate: "CASCADE",
+    onDelete: "SET NULL",
   },
   createdAt: {
     allowNull: false,
@@ -25,12 +27,29 @@ const OrderSchema = {
     field: "created_at",
     defaultValue: Sequelize.NOW,
   },
+  total: {
+    type: DataTypes.VIRTUAL,
+    get() {
+      if (this.items.length > 0) {
+        return this.items.reduce((total, item) => {
+          return total + item.price * item.OrderProduct.amount;
+        }, 0);
+      }
+      return 0;
+    },
+  },
 };
 
 class Order extends Model {
   static associate(models) {
     this.belongsTo(models.Customer, {
       as: "customer",
+    });
+    this.belongsToMany(models.Product, {
+      as: "items",
+      through: models.OrderProduct,
+      foreignKey: "orderId",
+      otherKey: "productId",
     });
   }
 
--- services/order.service.js
+++ services/order.service.js
@@ -10,19 +10,33 @@ class OrderService {
     return newOrder;
   }
 
+  async addItem(data) {
+    const newItem = await models.OrderProduct.create(data);
+    return newItem;
+  }
+
   async find() {
     return [];
   }
 
   async findOne(id) {
     const order = await models.Order.findByPk(id, {
-      include: [{ association: "customer", include: ["user"] }],
+      include: [
+        {
+          association: "customer",
+          include: ["user"],
+        },
+        "items",
+      ],
     });
     return order;
   }
 
   async update(id, changes) {
-    return { id, changes };
+    return {
+      id,
+      changes,
+    };
   }
 
   async delete(id) {
